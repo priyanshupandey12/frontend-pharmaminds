1386861aac2ea826bd68dd87a1d00d476fb1c725
priyanshupandey12 <129362864+priyanshupandey12@users.noreply.github.com>
2025-03-04 16:38:46 +0530

    Update courseApi.js

--- src/store/courseApi.js
+++ src/store/courseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "http://localhost:3000/api/v1/courses/";
+const COURSE_API = "https://backend-pharmaminds.onrender.com/api/v1/courses/";
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
